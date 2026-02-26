f91783b47b471c0e8e30add0bd8ac7b36525fd7b
marisolbernasconi30 <usa2018sb1030@gmail.com>
2026-02-25 23:54:49 -0300

    modifico el entity, corrijo el nombre de la tabla e importo los paquetes correspondientes

--- demo/src/main/java/com/example/demo/entity/Cliente.java
+++ demo/src/main/java/com/example/demo/entity/Cliente.java
@@ -1,7 +1,13 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 @Entity
-@Table(name = "clientes")
+@Table(name = "clientes_gimnasio")
 public class Cliente {
 
     @Id
