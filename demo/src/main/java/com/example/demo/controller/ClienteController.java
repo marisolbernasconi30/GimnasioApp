caad89f99714355c0efa7c860c5da5df6d2c1d79
marisolbernasconi30 <usa2018sb1030@gmail.com>
2026-02-25 23:05:48 -0300

    añadi los endpoints en el archivo del controlador principal, y cree sus metodos en el repositorio principal, todo del backend

--- demo/src/main/java/com/example/demo/controller/ClienteController.java
+++ demo/src/main/java/com/example/demo/controller/ClienteController.java
@@ -1,5 +1,36 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.Cliente;
+import com.example.demo.repository.ClienteRepository;
+
+@RestController
+@RequestMapping("/clientes")
+@CrossOrigin(origins = "http://localhost:5173") // esta anotacion es para habilitar el intercambio de recursos entre CORDS (origenes) en controladores REST. 
+                                               // permiten al navegador web solicitar recursos de un dominio diferente al servidor backend
 public class ClienteController {
 
+    private final ClienteRepository clienteRepository;
+
+    public ClienteController(ClienteRepository clienteRepository) {
+        this.clienteRepository = clienteRepository;
+    }
+
+    @GetMapping
+    public List<Cliente> listarClientes() {
+        return clienteRepository.findAll();
+    }
+
+    @PostMapping
+    public Cliente crearCliente(@RequestBody Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
 }
--- demo/src/main/java/com/example/demo/repository/ClienteRepository.java
+++ demo/src/main/java/com/example/demo/repository/ClienteRepository.java
@@ -1,5 +1,19 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
+import com.example.demo.entity.Cliente;
+
 public class ClienteRepository {
+
+    public List<Cliente> findAll() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+    }
+
+    public Cliente save(Cliente cliente) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'save'");
+    }
 
 }
