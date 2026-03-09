501b92d734525178e2bc4f50b5adc91be6403c92
marisolbernasconi30 <usa2018sb1030@gmail.com>
2026-03-09 00:18:44 -0300

    adelante unos endpoints

--- demo/src/main/java/com/example/demo/controller/ClienteController.java
+++ demo/src/main/java/com/example/demo/controller/ClienteController.java
@@ -2,7 +2,9 @@ package com.example.demo.controller;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entity.Cliente;
 import com.example.demo.repository.ClienteRepository;
+import com.example.demo.service.ClienteService;
+
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @RestController
 @RequestMapping("/clientes")
@@ -24,13 +31,27 @@ public class ClienteController {
         this.clienteRepository = clienteRepository;
     }
 
-    @GetMapping
+    @GetMapping //los metodos Get, son los que REGUPERAN los datos
     public List<Cliente> listarClientes() {
         return clienteRepository.findAll();
     }
 
-    @PostMapping
+    @PostMapping //el metodo Post CREA un nuevo recurso, se lo envia al servidor. 
     public Cliente crearCliente(@RequestBody Cliente cliente) {
         return clienteRepository.save(cliente);  //esto es a la vuelta (bbdd, entity, service, dto, controller, json, cliente), por eso usa el metodo save
     }
+
+    @PutMapping("path/{id}") //checkear si esto esta bien 
+    public String putMethodName(@PathVariable String id, @RequestBody String entity) {
+        // process PUT request
+
+        return entity;
+    }
+
+    @DeleteMapping("/clientes/eliminado") //asi se crea un delete 
+    public ResponseEntity<Void> eliminarClientes(@PathVariable Long id) {
+        ClienteService.eliminar(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
--- demo/src/main/java/com/example/demo/service/ClienteService.java
+++ demo/src/main/java/com/example/demo/service/ClienteService.java
@@ -2,4 +2,9 @@ package com.example.demo.service;
 
 public class ClienteService {
 
+    public static void eliminar(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'eliminar'");
+    }
+
 }
