8afc75ff26f23b8ddfc3e2b2945595f4578b171e
Marisol Bernasconi <mm.bernasconi@outlook.com>
2026-09-07 07:37:40 -0300

    refactor: export API_URL

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:8080";
+export const API_URL = "http://localhost:8080";
 
 export const CLIENTES_URL = `${API_URL}/clientes`;
 export const INSCRIPCIONES_URL = `${API_URL}/inscripciones`;
